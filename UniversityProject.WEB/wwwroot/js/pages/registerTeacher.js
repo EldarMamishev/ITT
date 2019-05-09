3f85d20d3070a0ad31cd70197967ff48aba95041
Vladyslav Serhiienko <holms9853@gmail.com>
2019-05-09 12:43:46 +0300

    Update datepicker

---
+++ UniversityProject.WEB/wwwroot/js/pages/registerTeacher.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $("#yearpicker").kendoDatePicker({
+        format: "MM/dd/yyyy"
+    });
+
+    $("#yearpicker").attr("readonly", true);
+
+    var datepicker = $("#yearpicker").data("kendoDatePicker");
+
+    $("#yearpicker").click(function () {
+        datepicker.open();
+    });
+});
